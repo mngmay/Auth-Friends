716c2e54ba4b7c33067aad6f20a1385bbf3b0b13
May Ng <mngmay@gmail.com>
2019-08-20 12:42:26 -0700

    Created credentials hook and login function to set token

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
-    <form>
-      <input type="text" name="username" placeholder="Username" />
-      <input type="password" name="password" placeholder="Password" />
+    <form onSubmit={login}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
       <button>Submit</button>
     </form>
   );
