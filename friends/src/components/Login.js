5c5dcc32750f3e3f82fa273443b84c92871a0593
May Ng <mngmay@gmail.com>
2019-08-20 12:51:09 -0700

    Added loading hook and short circuit for loading message

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -6,6 +6,7 @@ const Login = () => {
     username: "",
     password: ""
   });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = e => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
@@ -13,31 +14,39 @@ const Login = () => {
 
   const login = e => {
     e.preventDefault();
+    setLoading(true);
     axios
       .post("http://localhost:5000/api/login", credentials)
       .then(res => {
         console.log(res);
         localStorage.setItem("token", res.data.payload);
+        setLoading(false);
       })
-      .catch(err => console.log(err.response));
+      .catch(err => {
+        console.log(err.response);
+        setLoading(false);
+      });
   };
 
   return (
-    <form onSubmit={login}>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-      />
-      <button>Submit</button>
-    </form>
+    <div>
+      {loading && <div className="loading-msg">Loading...</div>}
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
